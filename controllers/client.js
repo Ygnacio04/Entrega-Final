ab3599512048601abbe243c55413ccc5d1607bcb
Ygnacio04 <i20ag04@gmail.com>
2025-05-05 12:00:42 +0200

    Get client y get clientS

--- controllers/client.js
+++ controllers/client.js
@@ -38,3 +38,59 @@ const createClient = async (req, res) => {
         handleHttpError(res, "ERROR_CREATE_CLIENT");
     }
 };
+
+/**
+* Obtener cliente por id
+* @param {Object} req
+* @param {Object} res
+*/
+
+const getClient = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const user = req.user;
+
+         // Buscar cliente por ID que pertenezca al usuario o su compañía
+        const client = await clientsModel.findOne({
+            _id: id,
+            $or: [
+                {createdBy: user._id},
+                {company: user.company?._id}
+            ]
+        });
+
+        if (!client) {
+            return handleHttpError(res, "CLIENT_NOT_FOUND", 404);
+        }
+
+    }catch(error){
+        console.log(error);
+        handleHttpError(res, "ERROR_GET_CLIENT");
+    }
+};
+
+/**
+ * Obtener todos los clientes del usuario o compañía
+ * @param {Object} req
+ * @param {Object} res
+ */
+
+const getClients = async (req, res) => {
+    try {
+        const user = req.user;
+
+        // Buscar clientes del usuario o de su compañía
+        const clients = await clientsModel.find({
+            $or: [
+                {createdBy: user._id},
+                {company: user.company?._id}
+            ]
+        });
+        
+        res.send({clients});
+
+    }catch(error){
+        console.log(error);
+        handleHttpError(res, "ERROR_GET_CLIENTS")
+    }
+};
